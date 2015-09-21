0b3489f4848392e446602ad52510ea130bdb7b2c
Andrew Bates <abates@omeganetserv.com>
2015-09-21 09:24:13 -0400

    Updates for MPEG-TS in RTP

--- testsink.sh
+++ testsink.sh
@@ -3,18 +3,24 @@
 SGP=$1
 
 if [ -s $SGP ] ; then
-  echo "usage: $0 <src@grp:audio_port:video_port>"
+  echo "usage: $0 <src@grp:port>"
   exit 1
 fi
 
 SG=`echo $SGP | awk -F: '{print \$1}'`
-AUDIO_PORT=`echo $SGP | awk -F: '{print \$2}'`
-VIDEO_PORT=`echo $SGP | awk -F: '{print \$3}'`
+PORT=`echo $SGP | awk -F: '{print \$2}'`
 
-echo "Joining $SG on ports $AUDIO_PORT and $VIDEO_PORT"
-gst-launch-1.0 udpsrc multicast-iface="eth0" uri="udp://$SG:$AUDIO_PORT" name="audiosrc" ! 'application/x-rtp,payload=96,encoding-name=AC3,clock-rate=44100' !\
-                 rtpac3depay ! ac3parse ! avdec_ac3 ! autoaudiosink \
-               udpsrc multicast-iface="eth0" uri="udp://$SG:$VIDEO_PORT" name="videosrc"  ! 'application/x-rtp,payload=96,encoding-name=H264' ! \
-                 rtph264depay ! h264parse ! avdec_h264 ! autovideosink 
+echo "Joining $SG on port $PORT"
+gst-launch-1.0 udpsrc multicast-iface="eth0" uri="udp://$SG:$PORT" ! \
+              'application/x-rtp,payload=33,encoding-name=MP2T,clock-rate=90000' ! \
+               rtpmp2tdepay ! tsdemux name="demux" \
+                 demux. ! ac3parse ! avdec_ac3 ! autoaudiosink \
+                 demux. ! h264parse ! avdec_h264 ! autovideosink
+
+  #'application/x-rtp,payload=96,encoding-name=H264' ! \
+#udpsrc multicast-iface="eth0" uri="udp://$SG:$AUDIO_PORT" name="audiosrc" ! 'application/x-rtp,payload=96,encoding-name=AC3,clock-rate=44100' !\
+                 #rtpac3depay ! ac3parse ! avdec_ac3 ! autoaudiosink \
+                 #rtph264depay ! h264parse ! avdec_h264 ! autovideosink 
 
 exit 0
+
